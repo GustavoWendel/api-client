33c88e9d76073ecf221784951618c8012484f2f3
GustavoWendel <gustavo_wendel@hotmail.com>
2022-10-06 20:05:39 -0300

    Seeding script

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,11 @@
--- INSERT INTO tb_client (name, cpf, income, birthDate, children) VALUES('Gustavo Wendel da Silva Borges', '98765432101', 4.890, TIMESTAMP WITH TIME ZONE '1994-07-20T10:30:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Edson Yuri da Paz', '925.700.379-58', 6000.00, TIMESTAMP WITH TIME ZONE '1999-03-19T10:23:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Cauê Oliver Cardoso', '663.364.309-87', 3500.00, TIMESTAMP WITH TIME ZONE '1999-04-15T15:23:07.12345Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Geraldo Pedro Henrique da Rocha', '614.114.309-13', 5200.00, TIMESTAMP WITH TIME ZONE '1999-08-08T16:23:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Augusto Carlos Eduardo Nicolas Moreira', '608.386.259-08', 5100.00, TIMESTAMP WITH TIME ZONE '2022-05-09T17:23:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Kaique Edson Drumond', '319.102.499-98', 5300.00, TIMESTAMP WITH TIME ZONE '1999-03-10T18:23:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Arthur Benedito Mateus Figueiredo', '250.049.239-73', 5400.00, TIMESTAMP WITH TIME ZONE '1999-05-15T19:23:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Murilo Oliver Almada', '001.123.456-78', 7000.00, TIMESTAMP WITH TIME ZONE '1999-04-25T20:23:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Thiago Kauê Benício Galvão', '689.748.099-15', 8000.00, TIMESTAMP WITH TIME ZONE '1999-01-23T21:23:07.12345Z', 5);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Marcos Ricardo Teixeira', '617.657.169-34', 9000.00, TIMESTAMP WITH TIME ZONE '1999-01-07T23:23:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ruan Alexandre Kevin Gomes', '598.945.699-94', 6000.00, TIMESTAMP WITH TIME ZONE '1999-07-20T00:23:07.12345Z', 1);
+
